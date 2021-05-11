a97551c74ca7c63912ed7a2efb2b8248b4cb78d1
antoine brivet <antoine.brivet@le-campus-numerique.fr>
2021-05-11 11:45:05 +0200

    Layout et redondance

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@extends('layout')
+
+@section('content')
+@parent
 <body>
 Fiche du produit{{ $id }}
 </body>
-</html>
+@endsection
